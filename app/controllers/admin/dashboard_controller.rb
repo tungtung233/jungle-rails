278a7e1201a4a30327cd63153e9517788289f719
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-08-19 01:13:36 +0000

    Admin Dashboard - show total counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,10 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV['ADMIN_USERNAME'], :password => ENV['ADMIN_PASSWORD']
 
   def show
+    @product_count = Product.all.count
+    @category_count = Category.all.count
+
+    puts @product_count
+    puts @category_count
   end
 end
