a09defd02bac3f984dccaef88903c71346027212
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-08-20 00:43:29 +0000

    make login/logout feature look nicer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+  # allows us to @current_user in our view files
   helper_method :current_user
 
   private
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-    # If the user exists AND the password entered is correct.
+    # If the user exists AND the password entered is correct
     if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user 
-      # logged in when they navigate around our website.
+      # Save the user id inside the browser cookie
       session[:user_id] = user.id
       redirect_to '/'
     else
